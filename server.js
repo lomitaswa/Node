a2b1a34f0c2c6c7b3f8e6597559360c1451e82e5
Lomitaswa Suna <lomitaswa@users.noreply.github.com>
2019-12-25 12:09:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const https = require('https');
+
+const BASE_URL = 'https://api.datamuse.com/words?sp=';
+function genAlphabetArray(charA, charZ) {
+    var latters = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
+    for (; i <= j; ++i) {
+        latters.push(String.fromCharCode(i));
+    }
+    return latters;
+};
+var latterArray = genAlphabetArray('a', 'z'); // ["a", ..., "z"]
+
+//console.log(latterArray);
+var newArray = [];
+const getLatters = async () => {
+    try {
+        for (var i=0; i<latterArray.length; i++){
+            for (var j=i,k=latterArray[i]; j<2; j++){
+                    var fetchData =  https.get(`${BASE_URL}/k`);
+                    var words = JSON.parse(fetchData);
+                    newArray.push(words.word);
+            };
+        };
+        return newArray;
+    } catch (error){
+        console.error("error")
+    }
+}
+getLatters();
+console.log(newArray);
